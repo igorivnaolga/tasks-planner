1936dbeebf68573d2fb6a932b0db7aecfd76d9ab
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-05-04 04:45:06 +0300

    add redux, actions

---
+++ src/redux/actions.js
@@ -0,0 +1,26 @@
+import { nanoid } from 'nanoid';
+
+export const addTask = text => {
+  return {
+    type: 'tasks/addTask',
+    payload: {
+      id: nanoid(),
+      completed: false,
+      text,
+    },
+  };
+};
+
+export const deleteTask = taskId => {
+  return {
+    type: 'tasks/deleteTask',
+    payload: taskId,
+  };
+};
+
+export const setStatusFilter = value => {
+  return {
+    type: 'filters/setStatusFilter',
+    payload: value,
+  };
+};
